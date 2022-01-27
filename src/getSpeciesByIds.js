9c8af90cf63b2106195e8bbae0bfd348d986b39b
Renata Dias <renata_dias96@live.com>
2022-01-27 16:25:25 -0300

    Finalizado requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  if (ids == null) { return [] };
-  const speciesIds = data.species.filter( (obj) => obj.id == ids );
+function getSpeciesByIds(ids, ...arg) {
+  if (ids == null) { return []; }
+  const speciesIds = data.species.filter((obj) => obj.id === ids || obj.id === arg);
   return speciesIds;
 }
-console.log(getSpeciesByIds( '0938aa23-f153-4937-9f88-4858b24d6bce',
-'e8481c1d-42ea-4610-8e11-1752cfc05a46',));
 
 module.exports = getSpeciesByIds;
