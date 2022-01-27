fe80c9d3fc2d63052d9ee2340f1b3889bf265782
Renata Dias <renata_dias96@live.com>
2022-01-27 20:22:06 -0300

    Finalizado requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const speciesName = data.species.filter( (element) => { element.name == animal });
-  const olderThan = speciesName.every( (spec) => { spec.residents.age >= age });
+  const speciesName = data.species.find((element) => element.name === animal);
+  const olderThan = speciesName.residents.every((obj) => obj.age >= age);
   return olderThan;
 }
-console.log(getAnimalsOlderThan('penguins', 10));
+
 module.exports = getAnimalsOlderThan;
