e98d0c863e728f11ba004ecdc0c4463533c030a1
Renata Dias <renata@pop-os.localdomain>
2022-02-10 18:38:36 -0300

    Finalizado requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  if (animal === null) {
-    const allSpecies = data.species.map((element) => { element.name && element.residents.length });
-    return allSpecies;
+  if (animal === undefined) {
+    const objSpecies = {};
+    data.species.forEach((obj) => { objSpecies[obj.name] = Number(`${obj.residents.length}`); });
+    return objSpecies;
+  }
+  const speciesName = data.species.filter((obj) => animal.specie === obj.name);
+  if (animal.sex) {
+    const specieResidents = speciesName.map((element) => element.residents);
+    const animalResident = specieResidents.find((obj) => obj);
+    const animalSex = animalResident.filter((obj) => animal.sex === obj.sex);
+    return animalSex.length;
+  }
+  if (animal.specie.length > 0) {
+    const countAnimalSex = speciesName.find((obj) => obj);
+    return countAnimalSex.residents.length;
   }
 }
-console.log(countAnimals());
-
 module.exports = countAnimals;
