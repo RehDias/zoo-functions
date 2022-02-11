dd48a690e36abe78b8db282adac1b58c58fbfc14
Renata Dias <renata@pop-os.localdomain>
2022-02-11 17:27:04 -0300

    Finalizado requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((element) => element.age < 18).length;
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50).length;
+  const senior = entrants.filter((element) => element.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
-}
+  if (entrants === undefined || Object.keys(entrants).length === 0) { return 0; }
+  const values = data.prices;
+  const pessoas = countEntrants(entrants);
+  return Object.entries(pessoas).reduce((acc, [type, count]) => { 
+    const totalValues = values[type] * count;
+    return totalValues + acc;
+   }, 0)
+} // Referência: Ajuda da Ellen Santos;
 
 module.exports = { calculateEntry, countEntrants };
